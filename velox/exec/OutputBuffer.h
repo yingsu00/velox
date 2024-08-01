bca61852a3f3f8a788215e877f7b69deb34fb9f1
yingsu00 <yingsu00@outlook.com>
2024-07-31 21:39:53 -0700

    Improve OutputBuffer blocking issue

--- velox/exec/OutputBuffer.h
+++ velox/exec/OutputBuffer.h
@@ -15,6 +15,8 @@
  */
 #pragma once
 
+#include <shared_mutex>
+
 #include "velox/core/PlanNode.h"
 #include "velox/exec/ExchangeQueue.h"
 
@@ -61,14 +63,18 @@ class ArbitraryBuffer {
  public:
   /// Returns true if this arbitrary buffer has no buffered pages.
   bool empty() const {
-    return pages_.empty() || (pages_.size() == 1 && pages_.back() == nullptr);
+    return pages_.withRLock([](auto& pages) {
+      return pages.empty() || (pages.size() == 1 && pages.back() == nullptr);
+    });
   }
 
   /// Returns true if this arbitrary buffer will not receive any new pages from
   /// enqueue() but it can still has buffered pages waiting to dispatch to
   /// destination on data fetch.
   bool hasNoMoreData() const {
-    return !pages_.empty() && (pages_.back() == nullptr);
+    return pages_.withRLock([](auto& pages) {
+      return !pages.empty() && (pages.back() == nullptr);
+    });
   }
 
   /// Marks this arbitrary buffer will not receive any new incoming pages. It
@@ -87,7 +93,9 @@ class ArbitraryBuffer {
   std::string toString() const;
 
  private:
-  std::deque<std::shared_ptr<SerializedPage>> pages_;
+  folly::Synchronized<std::deque<std::shared_ptr<SerializedPage>>> pages_;
+  //  std::deque<std::shared_ptr<SerializedPage>> pages_;
+  //  std::shared_mutex sharedMutex_;
 };
 
 class DestinationBuffer {
@@ -98,11 +106,23 @@ class DestinationBuffer {
   /// 2. Sent: the data is removed from the buffer after it is acked or
   ///          deleted.
   struct Stats {
-    void recordEnqueue(const SerializedPage& data);
+    Stats() {}
 
-    void recordAcknowledge(const SerializedPage& data);
-
-    void recordDelete(const SerializedPage& data);
+    Stats(
+        bool _finished,
+        int64_t _bytesBuffered,
+        int64_t _rowsBuffered,
+        int64_t _pagesBuffered,
+        int64_t _bytesSent,
+        int64_t _rowsSent,
+        int64_t _pagesSent)
+        : finished(_finished),
+          bytesBuffered(_bytesBuffered),
+          rowsBuffered(_rowsBuffered),
+          pagesBuffered(_pagesBuffered),
+          bytesSent(_bytesSent),
+          rowsSent(_rowsSent),
+          pagesSent(_pagesSent) {}
 
     bool finished{false};
 
@@ -117,7 +137,7 @@ class DestinationBuffer {
     int64_t pagesSent{0};
   };
 
-  void enqueue(std::shared_ptr<SerializedPage> data);
+  void enqueue(std::shared_ptr<SerializedPage> newData);
 
   /// Invoked to load data with up to 'notifyMaxBytes_' bytes from arbitrary
   /// 'buffer' if there is pending fetch from this destination in which case
@@ -185,17 +205,36 @@ class DestinationBuffer {
   std::string toString();
 
  private:
+  bool validateSequenceNumber(const int64_t sequence, bool fromGetData);
+
   void clearNotify();
 
-  std::vector<std::shared_ptr<SerializedPage>> data_;
+  void recordEnqueue(const SerializedPage& data);
+
+  void recordAcknowledge(const SerializedPage& data);
+
+  void recordDelete(const SerializedPage& data);
+
+//  std::vector<std::shared_ptr<SerializedPage>> data_;
+  folly::Synchronized<std::vector<std::shared_ptr<SerializedPage>>> data_;
   // The sequence number of the first in 'data_'.
-  int64_t sequence_ = 0;
+  //  int64_t sequence_ = 0;
+  std::atomic<int64_t> sequence_ = 0;
+
   DataAvailableCallback notify_{nullptr};
   DataConsumerActiveCheckCallback aliveCheck_{nullptr};
   // The sequence number of the first item to pass to 'notify'.
   int64_t notifySequence_{0};
   uint64_t notifyMaxBytes_{0};
-  Stats stats_;
+  std::shared_mutex notifyMutex_;
+
+  std::atomic<bool> finished_ = false;
+  std::atomic<int64_t> bytesBuffered_{0};
+  std::atomic<int64_t> rowsBuffered_{0};
+  std::atomic<int64_t> pagesBuffered_{0};
+  std::atomic<int64_t> bytesSent_{0};
+  std::atomic<int64_t> rowsSent_{0};
+  std::atomic<int64_t> pagesSent_{0};
 };
 
 class Task;
@@ -335,7 +374,7 @@ class OutputBuffer {
 
   void updateTotalBufferedBytesMsLocked();
 
-  int64_t getAverageBufferTimeMsLocked() const;
+  int64_t getAverageBufferTimeMsLocked();
 
   // If this is called due to a driver processed all its data (no more data),
   // we increment the number of finished drivers. If it is called due to us
@@ -379,7 +418,13 @@ class OutputBuffer {
     return kind_ == core::PartitionedOutputNode::Kind::kArbitrary;
   }
 
-  const std::shared_ptr<Task> task_;
+  FOLLY_ALWAYS_INLINE uint64_t buffersSize() {
+    return buffers_.withRLock([&](auto& buffers) { return buffers.size(); });
+  }
+
+  DestinationBuffer* safeGetBuffer(int destination);
+
+      const std::shared_ptr<Task> task_;
   const core::PartitionedOutputNode::Kind kind_;
   /// If 'totalSize_' > 'maxSize_', each producer is blocked after adding
   /// data.
@@ -388,51 +433,82 @@ class OutputBuffer {
   // resumed.
   const uint64_t continueSize_;
   const std::unique_ptr<ArbitraryBuffer> arbitraryBuffer_;
+  // The next buffer index in 'buffers_' to load data from arbitrary buffer
+  // which is only used by arbitrary output type.
+  int32_t nextArbitraryLoadBufferIndex_{0};
+  //  std::atomic<int32_t> nextArbitraryLoadBufferIndex_{0};
+  // One buffer per destination.
+  //  std::vector<std::unique_ptr<DestinationBuffer>> buffers_;
+  folly::Synchronized<std::vector<std::unique_ptr<DestinationBuffer>>> buffers_;
 
   // Total number of drivers expected to produce results. This number will
   // decrease in the end of grouped execution, when we understand the real
   // number of producer drivers (depending on the number of split groups).
-  uint32_t numDrivers_{0};
+  std::atomic<uint32_t> numDrivers_{0};
 
   // If true, then we don't allow to add new destination buffers. This only
   // applies for non-partitioned output buffer type.
-  bool noMoreBuffers_{false};
+  //  bool noMoreBuffers_{false};
+  std::atomic<bool> noMoreBuffers_{false};
+
+  //  /// The total number of bytes/rows/pages sent via this output buffer.
+  //  std::atomic<int64_t> totalBytesSent_{0};
+  //  std::atomic<int64_t> totalRowsSent_{0};
+  //  std::atomic<int64_t> totalPagesSent_{0};
+  //  /// Average time each piece of data has been buffered for in milliseconds.
+  //  std::atomic<int64_t> averageBufferTimeMs{0};
+  //  /// The number of largest buffers that handle 80% of the total data.
+  //  std::atomic<int32_t> numTopBuffers_{0};
+
+  // Actual data size in 'buffers_'.
+  std::atomic<int64_t> bufferedBytes_{0};
+  // The number of buffered pages which corresponds to 'bufferedBytes_'.
+  std::atomic<int64_t> bufferedPages_{0};
+  // The total number of output bytes, rows and pages.
+  std::atomic<uint64_t> numOutputBytes_{0};
+  std::atomic<uint64_t> numOutputRows_{0};
+  std::atomic<uint64_t> numOutputPages_{0};
+  // Time since last change in bufferedBytes_. Used to compute total time data
+  // is buffered. Ignored if bufferedBytes_ is zero.
+  std::atomic<uint64_t> bufferStartMs_;
+  // Total time data is buffered as bytes * time.
+  double totalBufferedBytesMs_;
 
   // While noMoreBuffers_ is false, stores the enqueued data to
   // broadcast to destinations that have not yet been initialized. Cleared
   // after receiving no-more-broadcast-buffers signal.
+//  std::vector<std::shared_ptr<SerializedPage>> dataToBroadcast_;
   std::vector<std::shared_ptr<SerializedPage>> dataToBroadcast_;
 
-  std::mutex mutex_;
-  // Actual data size in 'buffers_'.
-  int64_t bufferedBytes_{0};
-  // The number of buffered pages which corresponds to 'bufferedBytes_'.
-  int64_t bufferedPages_{0};
-  // The total number of output bytes, rows and pages.
-  uint64_t numOutputBytes_{0};
-  uint64_t numOutputRows_{0};
-  uint64_t numOutputPages_{0};
-  std::vector<ContinuePromise> promises_;
-  // The next buffer index in 'buffers_' to load data from arbitrary buffer
-  // which is only used by arbitrary output type.
-  int32_t nextArbitraryLoadBufferIndex_{0};
-  // One buffer per destination.
-  std::vector<std::unique_ptr<DestinationBuffer>> buffers_;
+  std::mutex finishMutex_;
+  std::shared_mutex bufferMsMutex_;
+  //  // Actual data size in 'buffers_'.
+  //  int64_t bufferedBytes_{0};
+  //  // The number of buffered pages which corresponds to 'bufferedBytes_'.
+  //  int64_t bufferedPages_{0};
+  //  // The total number of output bytes, rows and pages.
+  //  uint64_t numOutputBytes_{0};
+  //  uint64_t numOutputRows_{0};
+  //  uint64_t numOutputPages_{0};
+//  std::vector<ContinuePromise> promises_;
+  folly::Synchronized<std::vector<ContinuePromise>> promises_;
+
   // The sizes of buffers_ and finishedBufferStats_ are the same, but
   // finishedBufferStats_[i] is set if and only if buffers_[i] is null as
   // the buffer is finished and deleted.
   std::vector<DestinationBuffer::Stats> finishedBufferStats_;
-  uint32_t numFinished_{0};
+  std::atomic<uint32_t> numFinished_{0};
   // When this reaches buffers_.size(), 'this' can be freed.
-  int numFinalAcknowledges_ = 0;
-  bool atEnd_ = false;
+  std::atomic<int> numFinalAcknowledges_ = 0;
+  std::atomic<bool> atEnd_ = false;
 
-  // Time since last change in bufferedBytes_. Used to compute total time data
-  // is buffered. Ignored if bufferedBytes_ is zero.
-  uint64_t bufferStartMs_;
-
-  // Total time data is buffered as bytes * time.
-  double totalBufferedBytesMs_;
+  //  // Time since last change in bufferedBytes_. Used to compute total time
+  //  data
+  //  // is buffered. Ignored if bufferedBytes_ is zero.
+  //  uint64_t bufferStartMs_;
+  //
+  //  // Total time data is buffered as bytes * time.
+  //  double totalBufferedBytesMs_;
 };
 
 } // namespace facebook::velox::exec
