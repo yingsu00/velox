5fdea7e828d5b00133a5a69fdb07258677ede72f
yingsu00 <yingsu00@outlook.com>
2024-07-31 21:39:53 -0700

    Improve OutputBuffer blocking issue
    Fixed task not running when enqueueing
    
    Add print message

--- velox/exec/Driver.cpp
+++ velox/exec/Driver.cpp
@@ -504,6 +504,7 @@ StopReason Driver::runInternal(
     std::shared_ptr<Driver>& self,
     std::shared_ptr<BlockingState>& blockingState,
     RowVectorPtr& result) {
+  VLOG(3) << this << this->toString() << " Driver::runInternal begin. ";
   const auto now = getCurrentTimeMicro();
   const auto queuedTimeUs = now - queueTimeStartUs_;
   // Update the next operator's queueTime.
@@ -637,6 +638,10 @@ StopReason Driver::runInternal(
               nextOp,
               curOperatorId_ + 1,
               kOpMethodNeedsInput);
+          VLOG(3) << this
+                  << " Driver::runInternal 3 called needsInput on nextOp: "
+                  << nextOp->toString() << " needsInput: " << needsInput;
+
           if (needsInput) {
             uint64_t resultBytes = 0;
             RowVectorPtr intermediateResult;
@@ -715,6 +720,7 @@ StopReason Driver::runInternal(
                   op,
                   curOperatorId_,
                   kOpMethodIsBlocked);
+
               if (blockingReason_ != BlockingReason::kNotBlocked) {
                 blockedOperatorId_ = curOperatorId_;
                 checkIsBlockFutureValid(op, future);
@@ -729,6 +735,7 @@ StopReason Driver::runInternal(
                   op,
                   curOperatorId_,
                   kOpMethodIsFinished);
+
               if (finished) {
                 auto timer = createDeltaCpuWallTimer(
                     [op, this](const CpuWallTiming& elapsedTime) {
@@ -764,6 +771,9 @@ StopReason Driver::runInternal(
                 op,
                 curOperatorId_,
                 kOpMethodGetOutput);
+            VLOG(3) << this << " Driver::runInternal called getOutput on op: "
+                    << op->toString() << " result: " << result;
+
             if (result) {
               VELOX_CHECK(
                   result->size() > 0,
@@ -1034,7 +1044,7 @@ std::vector<Operator*> Driver::operators() const {
 std::string Driver::toString() const {
   std::stringstream out;
   out << "{Driver." << driverCtx()->pipelineId << "." << driverCtx()->driverId
-      << ": ";
+      << ", taskId: " << driverCtx()->task->taskId();
   if (state_.isTerminated) {
     out << "terminated, ";
   }
