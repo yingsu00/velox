77ebabb8f2007d9656d5ff2288bc97db19375abb
yingsu00 <yingsu00@outlook.com>
2026-06-09 23:22:12 -0700

    refactor(expression): Add V2 evaluator skeleton (step 2)
    Introduce the additive ExprV2 / EvalFrame / ExprRuntimeState /
    ExprEvaluatorV2 / ExprSetV2 surface described in the design doc as
    empty skeletons. All entry points are VELOX_NYI stubs; nothing routes
    through them yet.
    
    The five new types compile cleanly and link into velox_expression.
    V1 (Expr, ExprSet) remains the only evaluation path until later steps.
    
    See velox/docs/designs/expr-evaluation-refactor.md.

---
+++ velox/expression/ExprV2.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "velox/expression/ExprV2.h"
+
+#include "velox/common/base/Exceptions.h"
+
+namespace facebook::velox::exec {
+
+// Adapter implementation lands in step 3.  Until then, calling this is a
+// programmer error -- nothing should be constructing ExprV2 yet.
+std::shared_ptr<ExprV2> ExprV2::from(const std::shared_ptr<Expr>& /*expr*/) {
+  VELOX_NYI("ExprV2::from is implemented in step 3 of the refactor.");
+}
+
+} // namespace facebook::velox::exec
